6941704fff1405c327c97f9e384f2aa59e0fbef6
visualstudio <visualstudio@122-PC37>
2019-02-08 19:05:50 -0500

    commit6:10

--- c0727191__week05/Program.cs
+++ c0727191__week05/Program.cs
@@ -7,9 +7,73 @@ using System.Threading.Tasks;
 namespace c0727191__week05
 {
     class Program
+
     {
+
         static void Main(string[] args)
+
         {
+
+            Elevator e = new Elevator();
+
+            e.run();
+
         }
+
     }
+
+
+
+    class Node
+
+    {
+
+        public Node() { }
+
+        public Node floor;
+
+        public Node elevatorUp;
+
+        public string FloorNumber;
+
+    }
+
+
+
+    class Elevator
+
+    {
+
+        Node FirstFloor;
+
+        Node SecondFloor;
+
+        Node ThirdFloor;
+
+        Node FourthFloor;
+
+
+
+        public void run()
+
+        {
+
+            FirstFloor = new Node();
+
+            FirstFloor.FloorNumber = "First Floor";
+
+            Console.WriteLine("floor number is {0} ", FirstFloor.FloorNumber);
+
+            FirstFloor.elevatorUp = SecondFloor;
+
+            SecondFloor.FloorNumber = "Second Floor";
+
+
+
+            Console.WriteLine("floor number is {0} ", SecondFloor.FloorNumber);
+
+        }
+
+    }
+
 }
